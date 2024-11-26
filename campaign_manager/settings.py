da3db88b8ea7f9de7e2e7de2b78380d2cd40c51f
Anmol Sharma <anmolsharma009a@gmail.com>
2024-11-26 23:13:35 +0530

    allow Render host

--- campaign_manager/settings.py
+++ campaign_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['campaign-manager-twqi.onrender.com','127.0.0.1']
 
 
 # Application definition
